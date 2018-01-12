331fa4c26fbdff783a44c3488e153c34d9b51138
Motregun <slava.vacheslav200302@gmail.com>
2018-01-12 21:58:40 +0200

    update

--- Runannex/app/src/main/java/com/example/admin/runannex/MainActivity.java
+++ Runannex/app/src/main/java/com/example/admin/runannex/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.admin.runannex;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText weight = (EditText) findViewById(R.id.weight);
+        EditText years = (EditText) findViewById(R.id.years);
+        EditText name = (EditText) findViewById(R.id.name);
+        EditText growth = (EditText) findViewById(R.id.growth);
+        Button next = (Button) findViewById(R.id.next);
+        name.requestFocus();
     }
+
 }
