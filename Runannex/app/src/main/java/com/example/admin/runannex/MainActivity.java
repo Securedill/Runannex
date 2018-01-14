08b35e6e06d38957dec7e0ff6eeb3c9e45928adf
Motregun <slava.vacheslav200302@gmail.com>
2018-01-14 13:16:05 +0200

    update

--- Runannex/app/src/main/java/com/example/admin/runannex/MainActivity.java
+++ Runannex/app/src/main/java/com/example/admin/runannex/MainActivity.java
@@ -5,29 +5,27 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout.LayoutParams;
 import android.widget.TextView;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.EditText;
-
-public class MainActivity extends AppCompatActivity {
-    ActionBar actionbar;
-    TextView textview;
-    LayoutParams layoutparams;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        EditText weight = (EditText) findViewById(R.id.weight);
-        EditText years = (EditText) findViewById(R.id.years);
-        EditText name = (EditText) findViewById(R.id.name);
-        EditText growth = (EditText) findViewById(R.id.growth);
-        Button next = (Button) findViewById(R.id.next);
-        name.requestFocus();
-
+    public class MainActivity extends AppCompatActivity {
+            @Override
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
+                EditText weight = (EditText) findViewById(R.id.weight);
+                EditText year = (EditText) findViewById(R.id.year);
+                EditText name = (EditText) findViewById(R.id.name);
+                EditText growth = (EditText) findViewById(R.id.growth);
+                Button next = (Button) findViewById(R.id.next);
+                name.requestFocus();
     }
-
 }
