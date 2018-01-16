f87ee413fc79a1174df6572ca5bb6e7748f120b8
Motregun <slava.vacheslav200302@gmail.com>
2018-01-16 23:21:34 +0200

    update

--- Runannex/app/src/main/java/com/example/admin/runannex/MainActivity.java
+++ Runannex/app/src/main/java/com/example/admin/runannex/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.admin.runannex;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,7 +12,13 @@ import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
-            @Override
+    final String nam = "nae";
+    final String weigh = "weight";
+    final String growt = "growth";
+    final String yea = "year";
+    Editor ed;
+
+    @Override
             protected void onCreate(Bundle savedInstanceState) {
                 super.onCreate(savedInstanceState);
                 setContentView(R.layout.activity_main);
@@ -20,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
                 final EditText growth = (EditText) findViewById(R.id.growth);
                 final Button next = (Button) findViewById(R.id.next);
                 final TextView error = (TextView) findViewById(R.id.error);
+                SharedPreferences sPref = getApplicationContext().getSharedPreferences("Data", MODE_PRIVATE);
+                ed = sPref.edit();
                 name.requestFocus();
                 error.setVisibility(View.INVISIBLE);
                 View.OnClickListener oclBtnOk = new View.OnClickListener() {
@@ -33,20 +43,26 @@ public class MainActivity extends AppCompatActivity {
                             error.setVisibility(View.VISIBLE);
                         } else {
                             error.setVisibility(View.INVISIBLE);
-                            int g = Integer.parseInt(Growth);
-                            int w = Integer.parseInt(Weight);
-                            int y = Integer.parseInt(Year);
 
-                            if (g>0 && y>0 && w>0 && Year.charAt(0) != '0' && Weight.charAt(0) != '0' && Growth.charAt(0) != '0') {
+                            if (Year.charAt(0) != '-' && Weight.charAt(0) != '-' && Growth.charAt(0) != '-' && Year.charAt(0) != '0' && Weight.charAt(0) != '0' && Growth.charAt(0) != '0') {
                                 error.setVisibility(View.INVISIBLE);
-                                Intent intent = new Intent(getApplicationContext(), Training.class);
-                                MainActivity.this.startActivity(intent);
+                                ed.putString("yea", Year);
+                                ed.putString("growt", Growth);
+                                ed.putString("weigh",Weight);
+                                ed.putString(nam, Name);
+                                ed.commit();
+                                Intent myIntent = new Intent(v.getContext(), Training.class);
+                                startActivity(myIntent);
+
+
+
                             } else  { error.setText("Неправильныо введены данные");  error.setVisibility(View.VISIBLE);}
 
 
                         }
 
                     }
+
                 };
 
                 next.setOnClickListener(oclBtnOk);
--- Runannex/app/src/main/java/com/example/admin/runannex/Training.java
+++ Runannex/app/src/main/java/com/example/admin/runannex/Training.java
@@ -1,15 +1,32 @@
 package com.example.admin.runannex;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
 public class Training extends AppCompatActivity {
+    SharedPreferences sPref;
+    EditText test;
+    String weight,year,growth,name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_training);
+        sPref = getApplication().getSharedPreferences("Data", MODE_PRIVATE);
+        weight = sPref.getString("weigh", "");
+        name = sPref.getString("nam", "");
+        growth = sPref.getString("growt", "");
+        year = sPref.getString("yea", "");
+
+
+
 
     }
 }
