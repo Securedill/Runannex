395ddf8a577d788fe3b339610d2d86d80552a1b0
Motregun <slava.vacheslav200302@gmail.com>
2018-02-05 16:24:57 +0200

    chronometr

--- Runannex/app/src/main/java/com/example/admin/runannex/MainActivity.java
+++ Runannex/app/src/main/java/com/example/admin/runannex/MainActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
@@ -38,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
             protected void onCreate(Bundle savedInstanceState) {
                 super.onCreate(savedInstanceState);
                 setContentView(R.layout.activity_main);
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
                 sPref = getApplicationContext().getSharedPreferences("Data", MODE_PRIVATE);
                 ed = sPref.edit();
 
--- Runannex/app/src/main/java/com/example/admin/runannex/Training.java
+++ Runannex/app/src/main/java/com/example/admin/runannex/Training.java
@@ -2,13 +2,18 @@ package com.example.admin.runannex;
 
 
 import android.content.SharedPreferences;
-import android.net.Uri;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.SystemClock;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.ImageView;
+
 import java.io.File;
 
 
@@ -17,11 +22,12 @@ public class Training extends AppCompatActivity {
     SharedPreferences sPref;
     String weight,year,growth,name;
     ImageView imageView;
-
+    long timeWhenStopped = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_training);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.abs_layout);
         sPref = getApplication().getSharedPreferences("Data", MODE_PRIVATE);
@@ -32,14 +38,70 @@ public class Training extends AppCompatActivity {
         imageView = (ImageView) findViewById(R.id.image_view);
         String path = Environment.getExternalStorageDirectory().getPath();
         File f = new File(path + "/.Runannex/picture.png");
-
+        final Button start = (Button) findViewById(R.id.start);
+        final Button pause = (Button) findViewById(R.id.pause);
+        final Button stop = (Button) findViewById(R.id.stop);
+        final Button cont = (Button) findViewById(R.id.cont);
+        final Chronometer timer = (Chronometer) findViewById(R.id.chronometer);
 
 
         /* if(f.exists() && !f.isDirectory()) {
             imageView.setImageURI(Uri.parse(new File("file://" + path + "/.Runannex/picture.png").toString()));
         } else { imageView.setImageResource(R.drawable.ava);} */
 
+        View.OnClickListener oclBtnStart = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timer.setBase(SystemClock.elapsedRealtime());
+                timer.start();
+                start.setVisibility(View.INVISIBLE);
+                stop.setVisibility(View.VISIBLE);
+                pause.setVisibility(View.VISIBLE);
+            }
+        };
+        start.setOnClickListener(oclBtnStart);
 
+
+
+        View.OnClickListener oclBtnStop = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stop.setVisibility(View.INVISIBLE);
+                pause.setVisibility(View.INVISIBLE);
+                cont.setVisibility(View.INVISIBLE);
+                start.setVisibility(View.VISIBLE);
+                timer.setBase(SystemClock.elapsedRealtime());
+                timeWhenStopped = 0;
+                timer.stop();
+
+
+            }
+        };
+        stop.setOnClickListener(oclBtnStop);
+
+
+
+        View.OnClickListener oclBtnPause = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timeWhenStopped = timer.getBase() - SystemClock.elapsedRealtime();
+                timer.stop();
+                pause.setVisibility(View.INVISIBLE);
+                cont.setVisibility(View.VISIBLE);
+            }
+        };
+        pause.setOnClickListener(oclBtnPause);
+
+        View.OnClickListener oclBtnCont = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timer.setBase(SystemClock.elapsedRealtime() + timeWhenStopped);
+                timer.start();
+                cont.setVisibility(View.INVISIBLE);
+                pause.setVisibility(View.VISIBLE);
+            }
+        };
+        cont.setOnClickListener(oclBtnCont);
 
     }
 
